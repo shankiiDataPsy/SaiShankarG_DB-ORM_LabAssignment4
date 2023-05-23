51e5108c648117f12a6b09d0b4eb9865d84a5e88
shankiiDataPsy <91925660+shankiiDataPsy@users.noreply.github.com>
2023-05-23 19:26:26 +0530

    Update Q7.sql

--- Q7.sql
+++ Q7.sql
@@ -1,8 +1,5 @@
-
-(
 select p.PRO_ID, p.PRO_NAME, o.ORD_DATE from 
 product p 
 inner join supplier_pricing sp on sp.PRO_ID=p.PRO_ID 
 inner join `order` as o on o.PRICING_ID=sp.PRICING_ID
-where o.ORD_DATE>'2021-10-05'
-);
+where o.ORD_DATE>'2021-10-05';
